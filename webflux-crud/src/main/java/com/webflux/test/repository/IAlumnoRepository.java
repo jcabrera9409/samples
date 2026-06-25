e1fbb40e8b37416ba95df30b639ebdd3430b6add
Jose Cabrera <jose.cabrera.villarreal@bbva.com>
2026-06-25 16:33:43 -0500

    .

--- webflux-crud/src/main/java/com/webflux/test/repository/IAlumnoRepository.java
+++ webflux-crud/src/main/java/com/webflux/test/repository/IAlumnoRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import com.webflux.test.model.Alumno;
 
 public interface IAlumnoRepository extends R2dbcRepository<Alumno, Long> {
+    
 }
